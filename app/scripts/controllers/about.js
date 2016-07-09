50926ff11825d4d317d42fc13bc76a6ee0b2716f
GIT ADMIN <kat.bana204@gmail.com>
2016-07-09 14:24:58 +0200

    app initialize with yeoman

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name anAppApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the anAppApp
+ */
+angular.module('anAppApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name anAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the anAppApp
+ */
+angular.module('anAppApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
