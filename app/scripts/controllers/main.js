c9c789e3c6ef3dc651f691613b5c130faba6bd7a
GIT ADMIN <kat.bana204@gmail.com>
2016-07-09 20:07:10 +0200

    add glyphicons

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -4,7 +4,6 @@ angular.module('anApp')
   .controller('MainCtrl', function ($scope, $http) {
     $scope.sortType = "forks_count";
     $scope.sortReverse = "false";
-    //$scope.searchForks = '';
 
     $http.get('https://api.github.com/search/repositories?q=user:X-Formation+fork:true').then(function(response){
     	$scope.repos = response.data.items;
