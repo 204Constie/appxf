a776c64e6f84650e60d73193335b8dcffb0270b5
GIT ADMIN <kat.bana204@gmail.com>
2016-07-10 16:57:34 +0200

    create a service

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,13 +1,31 @@
 'use strict';
 
 angular.module('anApp')
-  .controller('MainCtrl', function ($scope, $http) {
+  .service('githubService', function($http){
+ 	var githubService = {
+ 		async: function() {
+	 		var promise = $http.get('https://api.github.com/search/repositories?q=user:X-Formation+fork:true').then(function(response){
+			    	
+			    	console.log(response.data.items);
+			    	return response.data.items;
+			    });
+			 	return promise;
+			 }
+			};
+	return githubService;
+  
+  })
+  .controller('MainCtrl', function ($scope, githubService) {
     $scope.sortType = "forks_count";
     $scope.sortReverse = "false";
 
-    $http.get('https://api.github.com/search/repositories?q=user:X-Formation+fork:true').then(function(response){
-    	$scope.repos = response.data.items;
-    	console.log($scope.repos);
+    githubService.async().then(function(response){
+    	$scope.repos = response;
     });
-    
+
+    // if service was not made separately:
+    // $http.get('https://api.github.com/search/repositories?q=user:X-Formation+fork:true').then(function(response){
+    // 	$scope.repos = response.data.items;
+    // 	console.log($scope.repos);
+    // });
   });
