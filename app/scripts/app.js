7d6744db80085811e0e51574a1a696536ee6cfb6
GIT ADMIN <kat.bana204@gmail.com>
2016-07-14 14:44:11 +0200

    add resolve, make dynamic contributros

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -10,12 +10,30 @@ angular
       .when('/', {
         templateUrl: 'views/main.html',
         controller: 'MainCtrl',
-        // controllerAs: 'mainCtrl'
+        resolve: {
+            async: function($http) {
+              var promise = $http.get('https://api.github.com/search/repositories?q=user:X-Formation+fork:true').then(function(response){
+              
+              console.log(response.data.items);
+              return response.data.items;
+            });
+          return promise;
+         }
+        }
       })
       .when('/contributors', {
         templateUrl: 'views/contributors.html',
         controller: 'ContributorsCtrl',
-        // controllerAs: 'about'
+        resolve: {
+            async: function($http) {
+              var promise = $http.get('https://www.x-formation.com/wp-content/uploads/2014/09/contributors.json').then(function(response){
+              
+              console.log(response.data);
+              return response.data;
+            });
+          return promise;
+         }
+        }
       })
       .otherwise({
         redirectTo: '/'
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,31 +1,10 @@
 'use strict';
 
 angular.module('anApp')
-  .service('githubService', function($http){
- 	var githubService = {
- 		async: function() {
-	 		var promise = $http.get('https://api.github.com/search/repositories?q=user:X-Formation+fork:true').then(function(response){
-			    	
-			    	console.log(response.data.items);
-			    	return response.data.items;
-			    });
-			 	return promise;
-			 }
-			};
-	return githubService;
-  
-  })
-  .controller('MainCtrl', function ($scope, githubService) {
+  .controller('MainCtrl', function ($scope, async) {
     $scope.sortType = 'forks_count';
     $scope.sortReverse = 'false';
 
-    githubService.async().then(function(response){
-    	$scope.repos = response;
-    });
+    $scope.repos = async;
 
-    // if service was not made separately:
-    // $http.get('https://api.github.com/search/repositories?q=user:X-Formation+fork:true').then(function(response){
-    // 	$scope.repos = response.data.items;
-    // 	console.log($scope.repos);
-    // });
   });
